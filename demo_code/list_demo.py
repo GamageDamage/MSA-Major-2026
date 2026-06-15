930d440bc1af1a77521314c907dd7cc56700dc5a
GamageDamage <evinmetmira@gmail.com>
2026-06-15 11:29:19 -0500

    Add files via upload

---
+++ demo_code/list_demo.py
@@ -0,0 +1,12 @@
+def main():
+    #create a list of strings, ints, and diff vals
+    names = ["John", "mary", "Alice", "Bob"]
+    list_of_ints = [10,16,24,42,14,9]
+    random_type_list = ["Cyd",15,22.3,True,"Frank"]
+    print(list_of_ints)
+
+    print("\nAdding Valujes to a List:\n-----------------------------------")
+    names.append("james")
+    list_of_ints.append(63)
+    list_of_ints.append(5)
+    print(f"List of integers: {list_of_ints}")
