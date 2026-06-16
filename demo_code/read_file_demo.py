84b808b29e1e83b0f0c2cc7f792b40fefa9f0216
GamageDamage <evinmetmira@gmail.com>
2026-06-16 11:30:14 -0500

    Add files via upload

---
+++ demo_code/read_file_demo.py
@@ -0,0 +1,15 @@
+#open menu.txt: create a file handler to open file in read mode
+#create a empty dict
+#use a loop to read the filer line by line
+    #split the line at the comma
+    #get the item and price from the list
+    #create an entry in the dict for the item and price
+
+def main():
+    data_file = open("menu.txt", "r")
+    menu_items = {}:
+    for line_of_date in data_file:
+        
+
+
+main()
